15197082a8d9dc995dd90c9c4d4fdfc93dfe2ece
chenskiy_vladislav <chenskiy.vlad@mail.ru>
2022-05-25 06:38:15 +0300

    Редактирование и тестирование

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,8 +1,18 @@
-﻿string[] LimitedCharacters(string[] array)
+﻿void PrintArray(string[] arr)                        // Метод для вывода на печать массива
+{
+    Console.Write($"[ ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.Write($"]");
+}
+
+string[] LimitedCharacters(string[] array)           // Метод для решения задачи
 {
     int symbolLength = 3;
     int count = 0;
-    for (int i = 0; i < array.Length; i++)
+    for (int i = 0; i < array.Length; i++)           // Считаем количество подходящих элементов            
     {
         if (array[i].Length <= symbolLength)
         {
@@ -10,20 +20,24 @@
         }
     }
 
-    string[] newArray = new string[count];
+    string[] newArray = new string[count];          // Создаем новый массив для заполнения подходящими элементами 
     int j = 0;
 
-    for (int i = 0; i < array.Length; i++)
+    for (int i = 0; i < array.Length; i++)          // Заполняем массив подходящими элементами
     {
         if (array[i].Length <= symbolLength)
         {
             newArray[j] = array[i];
-            Console.Write($"{newArray[j]} ");
             j++;
         }
     }
     return newArray;
 }
 
-string[] arrayInput = { "hello", "2", "world", ":-)", "1234", "1567", "-2", "computer scince", "Russia", "Denmark", "Kazan" };
-LimitedCharacters(arrayInput);
+string[] arrayInput = { "hello", "2", "world", ":-)", "1234", "1567", "-2", "computer scince", "Rus", "Denmark", "Kazan" };
+Console.WriteLine($"Начальный массив:");
+PrintArray(arrayInput);
+string[] formedArray = LimitedCharacters(arrayInput);
+Console.WriteLine();
+Console.WriteLine($"Сформированный массив:");
+PrintArray(formedArray);
