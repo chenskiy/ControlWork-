ad0136f09c153916b43d3933f57b395ea4c20754
chenskiy_vladislav <chenskiy.vlad@mail.ru>
2022-05-25 05:29:14 +0300

    Поставленная задача решина

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] LimitedCharacters(string[] array)
+{
+    int symbolLength = 3;
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= symbolLength)
+        {
+            count++;
+        }
+    }
+
+    string[] newArray = new string[count];
+    int j = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= symbolLength)
+        {
+            newArray[j] = array[i];
+            Console.Write($"{newArray[j]} ");
+            j++;
+        }
+    }
+    return newArray;
+}
+
+string[] arrayInput = { "hello", "2", "world", ":-)", "1234", "1567", "-2", "computer scince", "Russia", "Denmark", "Kazan" };
+LimitedCharacters(arrayInput);
